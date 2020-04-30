509cb7f1399cfb209ee2908147b6559b797350fc
mandavaramu <mandavaramu227@gmail.com>
2020-04-30 20:17:44 +0530

    Updates

---
+++ precache-manifest.c52b0d7b208ac4d18b4819b9cff1ab07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94acf7a065eab52815c74660d2812dd",
+    "url": "/badulla-profile/index.html"
+  },
+  {
+    "revision": "860cb49958ce6df055cc",
+    "url": "/badulla-profile/static/css/main.4bc37963.chunk.css"
+  },
+  {
+    "revision": "575e470a2a728e09094f",
+    "url": "/badulla-profile/static/js/2.a5fae8dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/badulla-profile/static/js/2.a5fae8dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "860cb49958ce6df055cc",
+    "url": "/badulla-profile/static/js/main.7ead45be.chunk.js"
+  },
+  {
+    "revision": "3fef7b50f1a5758a8dd6",
+    "url": "/badulla-profile/static/js/runtime-main.6af811fb.js"
+  }
+]);
